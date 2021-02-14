bc7c0655638cd1e749ac8c307e709a40c51dfab7
Clement Ip <clement.ip96@gmail.com>
2021-02-13 17:44:01 -0800

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/**
+* Template Name: iPortfolio - v1.5.1
+* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
+  "use strict";
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+})(jQuery);
