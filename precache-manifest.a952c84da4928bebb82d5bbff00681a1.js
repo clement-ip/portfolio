7b3ff4baaf6c690246dc9978de6dcaf8b0126e02
Clement Ip <clement.ip96@gmail.com>
2022-01-29 14:01:25 -0800

    Updates

---
+++ precache-manifest.a952c84da4928bebb82d5bbff00681a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7726a0d8d2aab52d764",
+    "url": "/static/css/main.8794ed36.chunk.css"
+  },
+  {
+    "revision": "e7726a0d8d2aab52d764",
+    "url": "/static/js/main.a4e77421.chunk.js"
+  },
+  {
+    "revision": "e77b415628cc798f0a13",
+    "url": "/static/js/runtime~main.f47575d0.js"
+  },
+  {
+    "revision": "8848959e1e69d2997986",
+    "url": "/static/css/2.e97aa100.chunk.css"
+  },
+  {
+    "revision": "8848959e1e69d2997986",
+    "url": "/static/js/2.af8c9316.chunk.js"
+  },
+  {
+    "revision": "636b45b9010e6bae4aad",
+    "url": "/static/js/3.67a8c9d2.chunk.js"
+  },
+  {
+    "revision": "3dca2084f3b0a7103d465bd0f89916a0",
+    "url": "/static/media/hero-bg.3dca2084.jpg"
+  },
+  {
+    "revision": "b5e3cc0c6aa650c9e3ba523df059dc17",
+    "url": "/static/media/boxicons.b5e3cc0c.woff2"
+  },
+  {
+    "revision": "86708f260a765d37cce51959dc4c097d",
+    "url": "/static/media/boxicons.86708f26.woff"
+  },
+  {
+    "revision": "342c527555cfdb9d35b5dd5629b3fa37",
+    "url": "/static/media/boxicons.342c5275.eot"
+  },
+  {
+    "revision": "8829e87f7fef25fef075707895401297",
+    "url": "/static/media/boxicons.8829e87f.ttf"
+  },
+  {
+    "revision": "dad92ba1a4eed23afdbd3df53657fe9a",
+    "url": "/static/media/boxicons.dad92ba1.svg"
+  },
+  {
+    "revision": "efd75e4d5269cfb70f7103a4a62af1d6",
+    "url": "/index.html"
+  }
+];
